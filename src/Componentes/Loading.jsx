d9725de329b12d204f516bebd2e5ad00f5d86cca
Renan <renan13.com@gmailcom>
2023-12-08 13:59:07 -0300

    corrigindo bugs 3

--- src/Componentes/Loading.jsx
+++ src/Componentes/Loading.jsx
@@ -1,10 +1,12 @@
+import svgLoading from '../img/loadingMovie.svg'
+
 
 import styles from './Loading.module.css'
 
 export default function Loading() {
   return (
       <div className={styles.boxLoading}>
-          <img src='../public/img/loadingMovie.svg' alt="Loading" />
+      <img src={svgLoading} alt="Loading" />
     </div>
   )
 }
